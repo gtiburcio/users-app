e943d4d87cd5ff1940fe6be000f330d57fb9c39e
gtiburcio_meli <guilherme.tiburcio@mercadolivre.com>
2023-03-26 15:57:40 -0300

    user controller

---
+++ src/main/kotlin/com/tiburcio/users/controllers/UserController.kt
@@ -0,0 +1,14 @@
+package com.tiburcio.users.controllers
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController {
+
+    @GetMapping
+    fun getUsers() = ResponseEntity.noContent().build<Void>()
+}
